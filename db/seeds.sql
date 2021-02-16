f60641e799d7651003ec81b5bde3a88e04272885
jenry valdes <jenryval93@gmail.com>
2021-02-16 00:41:05 -0600

    added sql database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("CheeseBurger")
+INSERT INTO burgers (burger_name) VALUES ("doubleCheeseBurger")
+INSERT INTO burgers (burger_name) VALUES ("veggieCheeseBurger")
