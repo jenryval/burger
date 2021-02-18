be9a2fa3f82f72750e088a9d661cf4b3563cf1d4
jenry valdes <jenryval93@gmail.com>
2021-02-17 18:38:26 -0600

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 const mysql = require('mysql');
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'burger_db'
+  });
+  
+}
 
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  Port     : 3000,
-  user     : 'root',
-  password : 'root',
-  database : 'burger_db'
-});
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root',
+//   database : 'burger_db'
+// });
 
 connection.connect( (err) => {
     if (err) {
